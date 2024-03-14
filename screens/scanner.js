dc1bf3cc9b3bd3663e3c313d3e6c9b9828771a8b
stanzin norzang <stanzinnorzang@stanzins-MacBook-Air.local>
2024-03-14 10:13:14 -0400

    Caches clear

--- screens/scanner.js
+++ screens/scanner.js
@@ -1,100 +1,60 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState, useRef } from "react";
 import {
-  View,
-  Text,
-  StyleSheet,
-  Button,
   SafeAreaView,
+  StyleSheet,
   Alert,
   TouchableOpacity,
+  View,
 } from "react-native";
-import { CameraView, useCameraPermissions } from "expo-camera/next";
-import { MaterialIcons } from "@expo/vector-icons";
+import BarcodeScanner from "../components/BarcodeScanner";
+import BrowserHeader from "../components/BrowserHeader";
+import { useNavigation } from "../context/NavigationContext"; // Adjust the path as necessary
 import WebView from "react-native-webview";
-import BrowserHeader from "../components/BrowserHeader"; // Assuming BrowserHeader is the correct component
+import { MaterialIcons } from "@expo/vector-icons";
 
 const Scanner = () => {
-  const [permission, requestPermission] = useCameraPermissions();
   const webViewRef = useRef(null);
+  const [webViewUrl, setWebViewUrl] = useState("https://google.com");
   const [cameraEnabled, setCameraEnabled] = useState(false);
-  const [webViewUrl, setWebViewUrl] = useState("https://www.google.com");
-  const [isScanningEnabled, setIsScanningEnabled] = useState(true);
+  const { updateUrl } = useNavigation();
 
-  const handleBarcodeScanned = ({ type, data }) => {
-    if (!isScanningEnabled || !data) return;
-
-    setIsScanningEnabled(false);
+  const handleScanData = (data) => {
     let newData =
       data.length === 13 && data.startsWith("0") ? data.substring(1) : data;
     console.log("Scanned data is here:", newData);
 
+    // Inject JavaScript into the WebView to fill the search input with scanned data
     const script = `
-    (function() {
-      var inputs = document.querySelectorAll("input[formControlName='search']");
-      var newData = '${newData}';
-      if(inputs.length > 0) {
-        var input = inputs[0];
-        input.value = newData;
-        input.dispatchEvent(new Event('input', { bubbles: true }));     
-        input.setAttribute('readonly', true);
-
-        // Clear the input after 3 seconds
-        setTimeout(function() {
-          input.value = "";
-          input.removeAttribute('readonly'); // Remove readonly attribute after clearing
-          input.dispatchEvent(new Event('input', { bubbles: true }));
-        }, 2000);
-      }
-    })();
-    true; 
-  `;
+      (function() {
+        var inputs = document.querySelectorAll("input[formControlName='search']");
+        var newData = '${newData}';
+        if(inputs.length > 0) {
+          var input = inputs[0];
+          input.value = newData;
+          input.dispatchEvent(new Event('input', { bubbles: true }));     
+          input.setAttribute('readonly', true);
+        }
+      })();
+      true; 
+    `;
     webViewRef.current?.injectJavaScript(script);
-    setTimeout(() => setIsScanningEnabled(true), 3000);
   };
 
   const handleNavigationStateChange = (navState) => {
-    const url = navState.url;
-    setWebViewUrl(url); // Update the URL bar with the current page's URL
-    const isScannerPage = url.endsWith("/cart/scanner");
+    const newUrl = navState.url;
+    setWebViewUrl(newUrl);
+    updateUrl(newUrl); // If using a navigation context to track URL changes
+    const isScannerPage = newUrl.includes("/cart/scanner");
     setCameraEnabled(isScannerPage);
   };
 
-  const onUrlSubmit = (url) => {
-    // Ensure the URL starts with 'http://' or 'https://'
-    const formattedUrl = url.match(/^http[s]?:\/\//) ? url : `https://${url}`;
-    setWebViewUrl(formattedUrl);
-  };
-
-  if (!permission?.granted) {
-    return (
-      <View style={styles.container}>
-        <Text>No access to camera</Text>
-        <Button title="Allow Camera" onPress={requestPermission} />
-      </View>
-    );
-  }
-
   return (
-    <SafeAreaView style={styles.safeAreaContainer}>
-      <BrowserHeader
-        onUrlSubmit={onUrlSubmit} // Update WebView URL when the user submits a new URL
-        inputValue={webViewUrl} // Pass the current URL to display in the URL bar
-      />
-      <View style={styles.container}>
-        {cameraEnabled && (
-          <CameraView
-            style={styles.camera}
-            onBarcodeScanned={handleBarcodeScanned}
-          >
-            <View style={styles.overlay}>
-              <View style={styles.frame} />
-            </View>
-          </CameraView>
-        )}
-      </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <BrowserHeader onUrlSubmit={setWebViewUrl} inputValue={webViewUrl} />
+      {cameraEnabled && <BarcodeScanner onScan={handleScanData} />}
       <WebView
         ref={webViewRef}
-        style={styles.webview}
+        style={{ flex: 1 }}
         source={{ uri: webViewUrl }}
         javaScriptEnabled={true}
         onNavigationStateChange={handleNavigationStateChange}
@@ -116,37 +76,6 @@ const Scanner = () => {
 };
 
 const styles = StyleSheet.create({
-  safeAreaContainer: {
-    flex: 1,
-    backgroundColor: "transparent",
-  },
-  container: {
-    alignItems: "center",
-  },
-  camera: {
-    width: "100%",
-    height: 100,
-  },
-  overlay: {
-    ...StyleSheet.absoluteFillObject,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  frame: {
-    width: 350,
-    height: 50,
-    borderWidth: 2,
-    borderColor: "#FF0000",
-    backgroundColor: "transparent",
-  },
-  buttonContainer: {
-    width: "90%",
-    position: "absolute",
-    backgroundColor: "orange",
-    bottom: 10,
-    alignSelf: "center",
-    borderRadius: 5,
-  },
   fab: {
     position: "absolute",
     width: 56,
@@ -158,9 +87,9 @@ const styles = StyleSheet.create({
     bottom: 20,
     shadowOpacity: 0.3,
     shadowRadius: 3,
-    shadowColor: "#000000",
+    shadowColor: "#000",
     shadowOffset: { height: 3, width: 0 },
-    elevation: 6, // for Android shadow
+    elevation: 6,
   },
   leftFab: {
     left: 20,
